0d55b07acf6a717c82eb5f963be04bf826ce47f3
alvaro212 <alvaro212@gmail.com>
2020-02-19 17:45:10 -0500

    change message to full name

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@ class HelloWorld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello, World AM"); 
+        System.out.println("Hello, World - Alvaro Mantilla"); 
     } 
 } 
